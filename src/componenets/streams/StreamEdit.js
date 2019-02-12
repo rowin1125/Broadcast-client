c8e17af09f90bc71a8ba0fd3fc6fef3b4be2b98a
Rowin Mol <rowin.mol@youngcapital.nl>
2019-02-12 19:13:11 +0100

    first push

---
+++ src/componenets/streams/StreamEdit.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
+
+class StreamEdit extends Component {
+  componentDidMount = () => {
+    this.props.fetchStream(this.props.match.params.id);
+  };
+
+  render() {
+    return <div>StreamEdit</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: (state.streams[ownProps.match.params.id])
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamEdit);
